9ecab7c4b163be36b9045df020344cc68508b649
piaan <VianRiztanta71@gmail.com>
2023-10-14 16:11:59 +0700

    fix dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -97,7 +97,6 @@
   <!--  Import Js Files -->
   <script src="{{asset('dist/libs/jquery/dist/jquery.min.js')}}"></script>
   <script src="{{asset('dist/libs/simplebar/dist/simplebar.min.js')}}"></script>
-  <script src="{{asset('dist/libs/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
   <!--  core files -->
   <script src="{{asset('dist/js/app.min.js')}}"></script>
   <script src="{{asset('dist/js/app.init.js')}}"></script>
