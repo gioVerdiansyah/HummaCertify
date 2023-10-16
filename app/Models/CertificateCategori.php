805952e4299973ed2cb18a7ae33bfd4eff156fda
Ahmad Ilyas Fitono <140124534+IlyasGaming@users.noreply.github.com>
2023-10-16 10:24:32 +0700

    Parent boot for uuid

--- app/Models/CertificateCategori.php
+++ app/Models/CertificateCategori.php
@@ -1,14 +1,17 @@
 <?php
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CertificateCategori extends Model
 {
     use HasFactory;
 
     public $incrementing = false, $keyType = "string";
+    protected static function boot() { parent::boot(); static::creating(function ($model) { if (empty($model->{$model->getKeyName()})) { $model->{$model->getKeyName()} = Str::uuid()->toString(); }}); }
+
     protected $fillable = [
         'name',
     ];
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Support\Str;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
     protected $table = 'users';
     public $incrementing = false, $keyType = "string";
+    protected static function boot() { parent::boot(); static::creating(function ($model) { if (empty($model->{$model->getKeyName()})) { $model->{$model->getKeyName()} = Str::uuid()->toString(); }}); }
 
     /**
      * The attributes that are mass assignable.
