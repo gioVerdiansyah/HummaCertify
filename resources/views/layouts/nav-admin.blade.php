7673dfad502106d964b8b71b5f2d4ece05714d85
Ahmad Ilyas Fitono <140124534+IlyasGaming@users.noreply.github.com>
2023-10-17 15:56:49 +0700

    Landing Page

--- resources/views/layouts/nav-admin.blade.php
+++ resources/views/layouts/nav-admin.blade.php
@@ -153,32 +153,22 @@
                         <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
                             <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Dashboards</span>
                         </a>
-                        <div class="collapse menu-dropdown" id="sidebarDashboards">
-                            <ul class="nav nav-sm flex-column">
-                                <li class="nav-item">
-                                    <a href="dashboard-analytics.html" class="nav-link" data-key="t-analytics"> Analytics </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="dashboard-crm.html" class="nav-link" data-key="t-crm"> CRM </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="index.html" class="nav-link" data-key="t-ecommerce"> Ecommerce </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="dashboard-crypto.html" class="nav-link" data-key="t-crypto"> Crypto </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="dashboard-projects.html" class="nav-link" data-key="t-projects"> Projects </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="dashboard-nft.html" class="nav-link" data-key="t-nft"> NFT</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="dashboard-job.html" class="nav-link" data-key="t-job">Job</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li> <!-- end Dashboard Menu -->
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
+                            <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Dashboards</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
+                            <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Dashboards</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
+                            <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Dashboards</span>
+                        </a>
+                    </li>
                 </ul>
             </div>
             <!-- Sidebar -->
