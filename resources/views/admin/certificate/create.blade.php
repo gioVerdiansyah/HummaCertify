308ede5da77e4f344bed12d15aa868267aa24729
Ahmad Ilyas Fitono <140124534+IlyasGaming@users.noreply.github.com>
2023-10-18 00:57:44 +0700

    Update tambah peserta

--- resources/views/admin/certificate/create.blade.php
+++ resources/views/admin/certificate/create.blade.php
@@ -21,13 +21,13 @@
                         </div>
                         <div class="col-6">
                             <div class="mb-4">
-                                <label for="firstNameinput" class="form-label">Nama Peserta</label>
+                                <label for="name" class="form-label">Nama Peserta</label>
                                 <input type="text" class="form-control" placeholder="Masukkan nama" name="name" value="{{ old('name') }}">
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="mb-4">
-                                <label for="lastNameinput" class="form-label">Kategori Sertifikat</label>
+                                <label for="certificate_categori_id" class="form-label">Kategori Sertifikat</label>
                                 <select name="certificate_categori_id" class="form-select">
                                     <option disabled selected>--Pilih Kategori--</option>
                                     @foreach ($categories as $category)
@@ -38,28 +38,53 @@
                         </div>
                         <div class="col-6">
                             <div class="mb-4">
-                                <label for="firstNameinput" class="form-label">NIS/NIM/NIP Peserta</label>
+                                <label for="password" class="form-label">NIS/NIM/NIP Peserta</label>
                                 <input type="text" class="form-control" placeholder="Masukkan NIS/NIM/NIP"
                                     name="password" value="{{ old('password') }}">
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="mb-4">
-                                <label for="firstNameinput" class="form-label">Bidang/Division</label>
+                                <label for="bidang" class="form-label">Bidang/Division</label>
                                 <input type="text" class="form-control" placeholder="Bidang yang diikuti peserta" name="bidang" value="{{ old('bidang') }}">
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="mb-3">
-                                <label for="firstNameinput" class="form-label">Email peserta (opsional)</label>
+                                <label for="email" class="form-label">Email peserta (opsional)</label>
                                 <input type="text" class="form-control" placeholder="Enter your firstname"
                                     name="email" value="{{ old('email') }}">
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="mb-4">
-                                <label for="lastNameinput" class="form-label">Tanggal Acara</label>
+                                <label for="" class="form-label">Sub Bidang</label>
+                                <input type="date" class="form-control" placeholder="Sub bidang peserta" name="" value="{{ old('') }}">
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="mb-4">
+                                <label for="" class="form-label">Tempat tanggal lahir</label>
+                                <input type="text" class="form-control" placeholder="Tempat dan tanggal lahir peserta" name="" value="{{ old('') }}">
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="mb-4">
+                                <label for="" class="form-label">Tanggal Acara</label>
                                 <input type="date" class="form-control" placeholder="dd/mm/yy" name="tanggal" value="{{ old('tanggal') }}">
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mb-4">
+                                <label for="" class="form-label">Predikat</label>
+                                <select name="" class="form-select">
+                                    <option disabled selected>Pilih Predikat...</option>
+                                    <option value="Predikat1">Predikat1</option>
+                                    <option value="Predikat2">Predikat2</option>
+                                    <option value="Predikat3">Predikat3</option>
+                                    <option value="Predikat4">Predikat4</option>
+                                    <option value="Predikat5">Predikat5</option>
+                                </select>
                             </div>
                         </div>
                         <div class="col-6">
