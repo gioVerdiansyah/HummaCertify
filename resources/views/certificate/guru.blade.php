a5efb6488deafaa69f03122d8c5b3f46af2f9258
piaan <VianRiztanta71@gmail.com>
2023-10-18 14:25:46 +0700

    guru certificate

--- resources/views/certificate/guru.blade.php
+++ resources/views/certificate/guru.blade.php
@@ -6,6 +6,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Laravel 7 PDF Example</title>
+
+  {{-- bootstrap icon --}}
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
+  {{-- font import --}}
+  <link
+    href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Merriweather:wght@400;700&family=Montserrat:wght@400;500;700&family=Open+Sans:wght@700&family=Playfair+Display:wght@400;700&family=Poppins:wght@400;500;700&display=swap"
+    rel="stylesheet">
 </head>
 
 <body>
@@ -43,7 +51,7 @@
         width: 100%;
         justify-content: center;
         display: flex;
-        top: 20px;
+        top: 40px;
       }
 
       .certificate-guru .content {
@@ -243,6 +251,17 @@
         font-weight: 700;
         font-size: 14.5px;
       }
+
+      .certificate-guru-belakang .content-belakang .table-penilayan table {
+        border: 1px solid
+      }
+      .certificate-guru-belakang .content-belakang .table-penilayan table thead {
+        border: 1px solid
+      }
+
+      th, td {
+  border: 1px solid black;
+}
     </style>
     <main>
       <div class="depan">
@@ -251,54 +270,57 @@
             <img src="{{ asset('image/certificate-guru.png') }}" alt="">
           </div>
           <div class="certificate-guru">
-            <div class="content">
-              <div class="top-text d-flex">
+            <div class="content" style="top: 20%;">
+              <div class="top-text d-flex" style="display: flex">
                 <div class="logo me-3">
                   <img src="{{ asset('image/logo-circle-humma.png') }}" alt="">
                 </div>
-                <div class="text">
-                  <p class="pt-humma">
+                <div class="text" style="margin-top: -35px">
+                  <p class="pt-humma" style=" font-family: 'Poppins', sans-serif;">
                     PT. Hummatech Digital Indonesia
                   </p>
-                  <p class="alamat">
+                  <p class="alamat" style="margin-bottom: 5px; margin-top: -5px">
                     Perum Permata Regency 1 Blok 10/28 Karangploso, Kabupaten Malang
                   </p>
-                  <div class="d-flex justify-content-center mt-1" style="gap: 0px 40px">
-                    <div class="icon"><i class="bi bi-globe me-2"></i>www.hummasoft.com</div>
-                    <div class="icon"><i class="bi bi-envelope-fill me-2"></i>info@hummasoft.com</div>
-                    <div class="icon"><i class="bi bi-telephone-fill me-2"></i>082132560566</div>
+                  <div class="d-flex justify-content-center mt-1"
+                    style="gap: 0px 40px; display: flex; font-family: 'Poppins', sans-serif; font-size: 13px">
+                    <div class="icon"><i class="bi bi-globe me-2" style="margin-right: 10px"></i>www.hummasoft.com
+                    </div>
+                    <div class="icon"><i class="bi bi-envelope-fill me-2"
+                        style="margin-right: 10px"></i>info@hummasoft.com</div>
+                    <div class="icon"><i class="bi bi-telephone-fill me-2" style="margin-right: 10px"></i>082132560566
+                    </div>
                   </div>
                 </div>
               </div>
               <hr>
               <div class="text-kopetensi mt-4">
-                <p class="sertifikat-kopetensi">
+                <p class="sertifikat-kopetensi" style="margin-bottom: 0">
                   Setifikat Kopetensi
                 </p>
-                <div class="number-certification mt-2">
+                <div class="number-certification mt-2"
+                  style="display: flex; align-content: center; justify-content: center">
                   <p class="me-4">No.</p>
                   {{-- Bisa diganti (Nomer Sertifikat) --}}
                   <p class="no-sertifikat">Ser/0003/03/omb/2023</p>
                 </div>
               </div>
-              <div class="desc mt-1">
-                Dengan ini menerangkan bahwa PT. Hummatech Digital Indonesia telah melaksanakan pelatihan
-                dan
-                uji kompetensi
-                "<span>Upskilling & Reskilling Guru Kejuruan Berstandar Industri Pola SMK PK-Magang
+              <div class="desc mt-1" style="font-size: 14px; line-height: 20px">
+                Dengan ini menerangkan bahwa PT. Hummatech Digital Indonesia telah melaksanakan pelatihan dan uji
+                kompetensi "<span>Upskilling & Reskilling Guru Kejuruan Berstandar Industri Pola SMK PK-Magang
                   Industri</span>" kepada:
               </div>
               <div class="identitas mt-3 row">
                 {{-- Bisa Diganti --}}
                 <div class="col-md-3 ms-3">
-                  <ul class="list-unstyled">
+                  <ul class="list-unstyled"style="list-style: none">
                     <li>Nama</li>
                     <li>NIK</li>
                     <li>Tempat, Tanggal Lahir</li>
                   </ul>
                 </div>
                 <div class="col-md-4">
-                  <ul class="list-unstyled">
+                  <ul class="list-unstyled"style="list-style: none">
                     {{-- Nama --}}
                     <li>: &nbsp;&nbsp; <span>Afri Mubarok, S.Kom</span></li>
                     {{-- NIK --}}
@@ -311,15 +333,15 @@
               <div class="text-telah">
                 <p>Telah dinyatakan kompeten dalam:</p>
               </div>
-              <div class="bidang row">
+              <div class="bidang row" style="display: flex">
                 <div class="col-md-3 ms-3">
-                  <ul class="list-unstyled">
-                    <li class="fw-bold">Bidang</li>
-                    <li class="fw-bold">Sub Bidang</li>
+                  <ul class="list-unstyled" style="list-style: none">
+                    <li class="fw-bold" style="font-weight: 700">Bidang</li>
+                    <li class="fw-bold" style="font-weight: 700">Sub Bidang</li>
                   </ul>
                 </div>
                 <div class="col-md-4">
-                  <ul class="list-unstyled">
+                  <ul class="list-unstyled" style="list-style: none">
                     {{-- Bidang --}}
                     <li>: &nbsp;&nbsp; Pemrograman Mobile App</li>
                     {{-- Sub Bidang --}}
@@ -327,12 +349,12 @@
                   </ul>
                 </div>
               </div>
-              <div class="bottom-finish row">
+              <div class="bottom-finish row" style="display: flex; justify-content:space-between">
                 <div class="col-md-6 ms-2 kiri">
                   <p style="margin-bottom: 0; font-size: 12px">DENGAN PREDIKAT</p>
                   <img style="width: auto;height: 130px" src="{{ asset('image/nilai-baik.png') }}" alt="">
                 </div>
-                <div class="col-md-5 kanan ms-5 ps-5">
+                <div class="col-md-5 kanan ms-5 ps-5" style="line-height: 5px">
                   <div class="atas">
                     <p>Ditetapkan di Malang</p>
                     {{-- Tanggal bisa dirubah --}}
@@ -357,77 +379,78 @@
           <div class="image-certificate-belakang">
             <img src="{{ asset('image/certificate-guru.png') }}" alt="">
           </div>
-          <div class="certificate-guru-belakang">
+          <div class="certificate-guru-belakang"
+            style="position: absolute; width: 100%; justify-content: center; display: flex; top: 120%;">
             <div class="content-belakang">
-              <div class="text-penilaian text-center">
+              <div class="text-penilaian text-center" style="text-align: center">
                 <p>PENILAIAN UJI KOMPETENSI</p>
                 <p>UPSKILLING & RESKILLING PEMROGRAMAN MOBILE APP</p>
               </div>
               <div class="table-penilayan mb-4">
-                <table class=" table-bordered">
+                <table style="border-collapse: collapse; width: 100%">
                   <thead>
-                    <tr class="text-center">
-                      <th class="col-1">No</th>
-                      <th class="col-8">Materi</th>
-                      <th class="col-2">Waktu</th>
+                    <tr style="text-align: center">
+                      <th colspan="1">No</th>
+                      <th colspan="1">Materi</th>
+                      <th colspan="1">Waktu</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
-                      <th class="text-center">1.</th>
+                      <th style="text-align: center">1.</th>
                       <td>Pengenalan dan Instalasi Flutter</td>
-                      <td class="text-center">8 JP</td>
+                      <td style="text-align: center">8 JP</td>
                     </tr>
                     <tr>
-                      <th class="text-center">2.</th>
+                      <th style="text-align: center">2.</th>
                       <td>Struktur Project Stateless dan Statefull Widget Flutter</td>
-                      <td class="text-center">8 JP</td>
+                      <td style="text-align: center">8 JP</td>
                     </tr>
                     <tr>
-                      <th class="text-center">3.</th>
+                      <th style="text-align: center">3.</th>
                       <td>Navigator dan Route di Flutter</td>
-                      <td class="text-center">8 JP</td>
+                      <td style="text-align: center">8 JP</td>
                     </tr>
                     <tr>
-                      <th class="text-center">4.</th>
+                      <th style="text-align: center">4.</th>
                       <td>Design UI/UX dengan Figma</td>
-                      <td class="text-center">8 JP</td>
+                      <td style="text-align: center">8 JP</td>
                     </tr>
                     <tr>
-                      <th class="text-center">5.</th>
+                      <th style="text-align: center">5.</th>
                       <td>Pengenalan GetX di Flutter</td>
-                      <td class="text-center">8 JP</td>
+                      <td style="text-align: center">8 JP</td>
                     </tr>
                     <tr>
-                      <th class="text-center">6.</th>
+                      <th style="text-align: center">6.</th>
                       <td>CRUD (Create, Read, Update, Delete) GetX dengan Firebase di Flutter</td>
-                      <td class="text-center">10 JP</td>
+                      <td style="text-align: center">10 JP</td>
                     </tr>
                     <tr>
-                      <th class="text-center">7.</th>
+                      <th style="text-align: center">7.</th>
                       <td>Uji Kompetensi Pemrograman Mobile App dengan Flutter</td>
-                      <td class="text-center">10 JP</td>
+                      <td style="text-align: center">10 JP</td>
                     </tr>
-                    <tr class="fw-bold">
-                      <th class="text-center"></th>
-                      <td class="text-center">Total</td>
-                      <td class="text-center">60 JP</td>
+                    <tr style="font-weight: 700">
+                      <th style="text-align: center"></th>
+                      <td style="text-align: center">Total</td>
+                      <td style="text-align: center">60 JP</td>
                     </tr>
                   </tbody>
                 </table>
               </div>
-              <div class="instruktur row">
+              <div class="instruktur" style="display: flex; justify-content: end">
                 <div class="col-md-8">
 
                 </div>
                 <div class="col-md-4">
-                  <div class="atas text-center">
+                  <div class="atas" style="text-align: center">
                     <p>Instruktur</p>
                   </div>
                   <div class="bawah text-center">
-                    <p class="nama-instruktur">Dito Cahya Pratama, S.Tr.Kom</p>
+                    <p class="nama-instruktur" style="margin-bottom: 4px">Dito Cahya Pratama, S.Tr.Kom</p>
                     <hr>
-                    <p>Senior Developer Hummatech</p>
+                    <p style="text-align: center; margin-top: 0px">Senior Developer Hummatech</p>
                   </div>
                 </div>
               </div>
