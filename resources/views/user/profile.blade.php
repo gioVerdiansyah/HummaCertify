c41cc41d292e58f514525c26741ec5cdff4f495f
piaan <VianRiztanta71@gmail.com>
2023-11-06 14:17:07 +0700

    fix layout profile & responsive

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -6,42 +6,53 @@
   <div class="pembungkus-profile">
     <div class="bababoi">
       <div class="row papope" style="width: 100%; height: 92vh; margin: 0">
-        <div class="col-md-8 p-0">
-          <div class="card card-profile p-4" style="height: 100%; border-top-left-radius: 0px; border-bottom-left-radius: 0px;">
+        <div class="col-md-12 col-lg-8 p-0">
+          <div class="card card-profile"
+            style="height: 100%; border-top-left-radius: 0px; border-bottom-left-radius: 0px;">
             <h3 class="profile-title mb-4">Profile</h3>
             <form action="{{ route('update.email') }}" method="POST" class="form-detail  emailForm">
               @csrf
               @method('PATCH')
               <div class="mb-3">
                 <label for="nama" class="form-label">Nama</label>
-                <input type="text" class="input-height form-control" id="nama" placeholder="{{ $user->name }}" readonly>
+                <input type="text" class="input-height form-control" id="nama" placeholder="{{ $user->name }}"
+                  readonly>
               </div>
-              <div class="input-group mb-5 flex-column">
-                <label for="email" class="form-label">Email</label>
-                <div class="d-flex mb-2">
-                  <input type="email" name="email" id="email" style="position: absolute;" class="input-height form-control rounded-start py-2" placeholder="{{ $user->email }}" value="{{ $user->email }}">
-                  <button type="submit" class="btn btn-primary btn-sm"><i class="fi fi-rr-pencil"></i></button>
+              <div class="input-button">
+                <div class="mb-3 button-width">
+                  <div class="input-email">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" name="email" id="email" class="input-height form-control"
+                      placeholder="{{ $user->email }}" value="{{ $user->email }}">
+                    @error('email')
+                      <p class="invalid mt-2 text-danger">{{ $message }}</p>
+                    @enderror
+                  </div>
                 </div>
-                @error('email')
-                    <p class="invalid">{{ $message }}</p>
-                @enderror
+                <div class="button-edit">
+                  <button type="submit" class="btn btn-primary"><i class="fi fi-rr-pencil"></i></button>
+                </div>
               </div>
+
               <div class="mb-3">
                 <label for="nik" class="form-label">Nisn/Nik/Nip</label>
-                <input type="number" class="input-height form-control" id="nik" placeholder="{{ $user->password }}" readonly>
+                <input type="number" class="input-height form-control" id="nik" placeholder="{{ $user->password }}"
+                  readonly>
               </div>
               <div class="mb-3">
                 <label for="institusi" class="form-label">Institusi</label>
-                <input type="text" class="input-height form-control" id="institusi" placeholder="{{ $user->institusi }}" readonly>
+                <input type="text" class="input-height form-control" id="institusi"
+                  placeholder="{{ $user->institusi }}" readonly>
               </div>
             </form>
           </div>
         </div>
-        <div class="col-md-4 p-0">
+        <div class="col-md-12 col-lg-4 p-0">
           <div class="card card-sertifikat-user" style="border-top-right-radius: 0px; border-bottom-right-radius: 0px;">
             <div class="card-body d-flex align-items-center">
               <div class="d-flex position-relative align-items-center">
-                <div class="content flex-shrink-0 me-3 avatar-xl rounded d-flex justify-content-center align-items-center" style="background-color: rgba(41, 186, 219, 23%);">
+                <div class="content flex-shrink-0 me-3 avatar-xl rounded d-flex justify-content-center align-items-center"
+                  style="background-color: rgba(41, 186, 219, 23%);">
                   <img src="{{ asset('image/certificate-vector.png') }}" style="width: 60px; height: auto" alt="">
                 </div>
                 <div class="text">
@@ -52,7 +63,8 @@
               </div>
             </div>
           </div>
-          <div class="card card-sertifikat-show p-4" style="border-top-right-radius: 0px; border-bottom-right-radius: 0px; margin: 2px 2px">
+          <div class="card card-sertifikat-show p-4"
+            style="border-top-right-radius: 0px; border-bottom-right-radius: 0px; margin: 2px 2px">
             <div class="mb-3">
               <h3 class="profile-title">Terbaru</h3>
             </div>
@@ -69,26 +81,31 @@
     </div>
   </div>
 
-  <div class="modal fade" id="fullscreenModal" tabindex="-1" aria-labelledby="fullscreeexampleModalLabel" aria-hidden="true">
+  <div class="modal fade" id="fullscreenModal" tabindex="-1" aria-labelledby="fullscreeexampleModalLabel"
+    aria-hidden="true">
     <div class="modal-dialog modal-fullscreen">
       <div class="modal-content">
         <div class="modal-body">
-          <iframe id="ifram" style="width: 98%;" src="{{ route('downloadCertificate', $certificate->id) }}" frameborder="0"></iframe>
-          <button type="button" class="btn-close" style="background-color: white" data-bs-dismiss="modal" aria-label="Close"></button>
+          <iframe id="ifram" style="width: 98%;" src="{{ route('downloadCertificate', $certificate->id) }}"
+            frameborder="0"></iframe>
+          <button type="button" class="btn-close" style="background-color: white" data-bs-dismiss="modal"
+            aria-label="Close"></button>
         </div>
       </div>
     </div>
   </div>
   @if (session('message'))
-		<script>
-				Swal.fire({
-						icon: "{{ session('message')['icon'] ?? 'success' }}",
-						title: "{{ session('message')['title'] ?? 'Oops' }}",
-						text: "{{ session('message')['text'] ?? 'Success' }}",
-				})
-		</script>
-	@endif
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.min.js" integrity="sha512-Z8CqofpIcnJN80feS2uccz+pXWgZzeKxDsDNMD/dJ6997/LSRY+W4NmEt9acwR+Gt9OHN0kkI1CTianCwoqcjQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+      Swal.fire({
+        icon: "{{ session('message')['icon'] ?? 'success' }}",
+        title: "{{ session('message')['title'] ?? 'Oops' }}",
+        text: "{{ session('message')['text'] ?? 'Success' }}",
+      })
+    </script>
+  @endif
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.min.js"
+    integrity="sha512-Z8CqofpIcnJN80feS2uccz+pXWgZzeKxDsDNMD/dJ6997/LSRY+W4NmEt9acwR+Gt9OHN0kkI1CTianCwoqcjQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script>
     var canvas = document.getElementById('pdfCanvas');
     var context = canvas.getContext('2d');
@@ -135,8 +152,8 @@
         event.preventDefault();
 
         Swal.fire({
-          title: 'anda yakin mau mengubah email anda?',
-          text: "tindakan ini tidak dapat di batalkan",
+          title: 'Anda yakin mau mengubah email anda?',
+          text: "Tindakan ini tidak dapat di batalkan",
           icon: "question",
           showCancelButton: true,
           confirmButtonText: "lanjutkan",
