2c63e52c4aa2717cc6fc8b1dafe7404b6fa5eeb6
Ahmad Ilyas Fitono <140124534+IlyasGaming@users.noreply.github.com>
2023-11-09 14:48:47 +0700

    Revert "Merge branch 'main' of https://github.com/gioVerdiansyah/HummaCertify"
    This reverts commit d76cc8f77b027df1eb3dcc14071bfc2b29dfcce4, reversing
    changes made to 5a824edfedb255cb0cdf0a31f75613c5e5937fdf.

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -80,7 +80,7 @@
                     <p class="text-danger">reCAPTCHA wajib diisi!</p>
                 @enderror
               </div>
-              <div class="mb-3 d-flex flex-row">
+              <div class="mb-3">
                 <button type="submit" class="btn btn-primary">Ganti Password</button>
                 <div class="lupa-password">
                   <a href="{{ route('password.request') }}">Lupa password?</a>
