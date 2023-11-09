8872df103944194df2f5a276f0b378749715aa3b
lintangpermata <permatalintang29@gmail.com>
2023-11-09 14:55:01 +0700

    tampilan email dll

--- resources/views/emails/kelulusan.blade.php
+++ resources/views/emails/kelulusan.blade.php
@@ -216,19 +216,10 @@
                                 <td class="t45" style="width:480px;">
                                   <p class="t51"
                                     style="margin:0;Margin:0;font-family:BlinkMacSystemFont,Segoe UI,Helvetica Neue,Arial,sans-serif,'Albert Sans';line-height:24px;font-weight:400;font-style:normal;font-size:14px;text-decoration:none;text-transform:none;direction:ltr;color:#09234F;text-align:left;mso-line-height-rule:exactly;mso-text-raise:3px;">
-                                    <span class="t131" style="margin:0;Margin:0;font-weight:700;mso-line-height-rule:exactly;">Selamat
-                                      atas kelulusan magang!</span> Kami ingin
-                                    menyampaikan apresiasi sebesar-besarnya
-                                    kepada
-                                    Anda atas dedikasi dan kerja keras selama
-                                    menjalani program magang di perusahaan
-                                    HummaTech. Perjalanan Anda selama magang
-                                    telah
-                                    membuktikan kemampuan, semangat, dan
-                                    komitmen
-                                    yang luar biasa, yang memberikan dampak
-                                    positif
-                                    dan berharga bagi perusahaan kami.
+                                    <span class="t131" style="margin:0;Margin:0;font-weight:700;mso-line-height-rule:exactly;">Selamat!</span> dengan ini kami berikan kepada anda sertifikat ... untuk menghargai 
+                                      dedikasi, kerja keras, dan semangat luar biasa anda selama di perusahaan kami. <br><br> Sertifikat ini bukanlah akhir dari pencapaian Anda, melainkan awal dari bab baru untuk masa depan Anda. Kami yakin bahwa bakat dan 
+                                      keterampilan yang Anda tunjukkan selama magang akan membawa Anda menuju kesuksesan. Teruslah mengejar impian dan berjuang 
+                                      untuk menjadi versi terbaik dari diri Anda.
                                   </p>
                                 </td>
                               </tr>
@@ -248,18 +239,9 @@
                                 <td class="t55" style="width:480px;padding:0 0 1px 0;">
                                   <p class="t61"
                                     style="margin:0;Margin:0;font-family:BlinkMacSystemFont,Segoe UI,Helvetica Neue,Arial,sans-serif,'Albert Sans';line-height:24px;font-weight:400;font-style:normal;font-size:14px;text-decoration:none;text-transform:none;direction:ltr;color:#09234F;text-align:left;mso-line-height-rule:exactly;mso-text-raise:3px;">
-                                    Sertifikat ini adalah tanda penghargaan dari
-                                    kami, yang dapat Anda gunakan untuk melamar
-                                    pekerjaan di berbagai perusahaan terkemuka.
-                                    Kami
-                                    yakin bahwa bakat dan pengetahuan yang Anda
-                                    tunjukkan selama magang akan membawa Anda
-                                    jauh
-                                    dalam karier Anda. Teruslah berjuang dan
-                                    terus
-                                    berkembang, dan semoga Anda mencapai
-                                    kesuksesan
-                                    yang luar biasa di masa depan. Selamat!</p>
+                                    Anda bisa menemukan lampiran sertifikat Anda pada bagian bawah email ini. Kami berharap bahwa sertifikat ini tidak hanya menjadi 
+                                    penghargaan atas upaya Anda, tetapi juga menjadi dorongan untuk terus berkembang dan meraih kesuksesan di masa mendatang. Semoga 
+                                    sertifikat ini menjadi tonggak awal menuju perjalanan yang penuh prestasi dan keberhasilan. Selamat dan sukses selalu!</p>
                                 </td>
                               </tr>
                             </table>
@@ -278,7 +260,7 @@
                                 <td class="t135" style="width:480px;padding:0 0 1px 0;">
                                   <p class="t141"
                                     style="margin:0;Margin:0;font-family:BlinkMacSystemFont,Segoe UI,Helvetica Neue,Arial,sans-serif,'Albert Sans';line-height:24px;font-weight:400;font-style:normal;font-size:14px;text-decoration:none;text-transform:none;direction:ltr;color:#09234F;text-align:left;mso-line-height-rule:exactly;mso-text-raise:3px;">
-                                    Jika ada kesalahan pada PDF kamu mungkin
+                                    Jika ada kesalahan pada PDF anda mungkin
                                     bisa
                                     mengunjungi link dibawah ini:</p>
                                 </td>
@@ -319,7 +301,7 @@
                                 <td class="t145" style="width:480px;padding:0 0 1px 0;">
                                   <p class="t151"
                                     style="margin:0;Margin:0;font-family:BlinkMacSystemFont,Segoe UI,Helvetica Neue,Arial,sans-serif,'Albert Sans';line-height:24px;font-weight:400;font-style:normal;font-size:14px;text-decoration:none;text-transform:none;direction:ltr;color:#09234F;text-align:left;mso-line-height-rule:exactly;mso-text-raise:3px;">
-                                    JIka masih ada kesalahan maka bisa hubungi
+                                    Jika masih ada kesalahan maka anda bisa hubungi
                                     kontak di bawah ini</p>
                                 </td>
                               </tr>
@@ -339,7 +321,7 @@
                                 <td class="t65" style="width:480px;">
                                   <p class="t71"
                                     style="margin:0;Margin:0;font-family:BlinkMacSystemFont,Segoe UI,Helvetica Neue,Arial,sans-serif,'Albert Sans';line-height:24px;font-weight:400;font-style:normal;font-size:14px;text-decoration:none;text-transform:none;direction:ltr;color:#09234F;text-align:left;mso-line-height-rule:exactly;mso-text-raise:3px;">
-                                    Thanks</p>
+                                    Terimakasih</p>
                                 </td>
                               </tr>
                             </table>
