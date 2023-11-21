715a29ec83ec6ca1fa46eacd8c487ea3a0ab8108
Ahmad Ilyas Fitono <140124534+IlyasGaming@users.noreply.github.com>
2023-11-21 15:33:20 +0700

    Flash nya balik lagi

--- public/js/loginLoader.js
+++ public/js/loginLoader.js
@@ -4,7 +4,6 @@ if (bgLoader == 'dark') {
     var eyeBackground = document.getElementById('eyeShow');
     var bgImage = document.getElementById('background-login');
     var icon = document.getElementById('icon');
-    bgImage.style = 'background-image: none !important';
     icon.style = 'color: white';
 }
 
